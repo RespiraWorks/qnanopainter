669d82482f205cb108e0499c97d879875b320e84
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-10-15 01:00:41 -0700

    "install and uninstall targets; updates #53"

--- cmake/CompilerConfig.cmake
+++ cmake/CompilerConfig.cmake
@@ -7,6 +7,7 @@ endif ()
 
 include (CheckCXXCompilerFlag)
 
+# Warnings
 check_cxx_compiler_flag ("-Wall" Wall_FLAG_SUPPORTED)
 if (Wall_FLAG_SUPPORTED)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
@@ -23,7 +24,6 @@ if (Wextra_FLAG_SUPPORTED)
 endif (Wextra_FLAG_SUPPORTED)
 
 if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
-  # Warnings
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
 
   # Optimizations
