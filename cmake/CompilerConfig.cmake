92630b711270e46812bd3c272a66a7c5b9771a9a
Martin Shetty <1972005+martukas@users.noreply.github.com>
2021-10-14 20:57:32 -0700

    "agnostic to qt version, autodetects Quick and Widgets; updates #53"

--- cmake/CompilerConfig.cmake
+++ cmake/CompilerConfig.cmake
@@ -5,9 +5,25 @@ if (NOT CMAKE_CXX_STANDARD OR CMAKE_CXX_STANDARD LESS 11)
   set(CMAKE_CXX_STANDARD 11)
 endif ()
 
+include (CheckCXXCompilerFlag)
+
+check_cxx_compiler_flag ("-Wall" Wall_FLAG_SUPPORTED)
+if (Wall_FLAG_SUPPORTED)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+endif (Wall_FLAG_SUPPORTED)
+
+check_cxx_compiler_flag ("-pedantic" pedantic_FLAG_SUPPORTED)
+if (pedantic_FLAG_SUPPORTED)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic")
+endif (pedantic_FLAG_SUPPORTED)
+
+check_cxx_compiler_flag ("-Wextra" Wextra_FLAG_SUPPORTED)
+if (Wextra_FLAG_SUPPORTED)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
+endif (Wextra_FLAG_SUPPORTED)
+
 if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
   # Warnings
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -Wextra")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g")
 
   # Optimizations
